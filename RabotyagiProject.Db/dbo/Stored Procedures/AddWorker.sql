3ccb854364f691b98def456529f000bde6197e7d
GothicXSPB <ZZZerggg@mail.ru>
2023-01-30 13:34:15 +0300

    Add procedure AddWorker

---
+++ RabotyagiProject.Db/dbo/Stored Procedures/AddWorker.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE AddWorker
+(
+@Name NVARCHAR(50),
+@Phone NVARCHAR(11),
+@Mail NVARCHAR(50)
+)
+AS
+INSERT INTO [Worker] ([Name],[Phone],[Mail])
+VALUES (@Name, @Phone, @Mail)
+
+
