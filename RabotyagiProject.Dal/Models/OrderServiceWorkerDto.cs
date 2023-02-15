7977ec37541450937c934b188100f8e5d336bbbb
DariaKostrik <dolorez0927@yandex.ru>
2023-02-15 22:58:00 +0300

    add Dto,model and Procedure For deleting

---
+++ RabotyagiProject.Dal/Models/OrderServiceWorkerDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabotyagiProject.Dal.Models
+{
+    public class OrderServiceWorkerDto
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ServiceId { get; set; }
+        public int? WorkerId { get; set; }
+        public int Workload { get; set; }
+
+    }
+}
---
+++ RabotyagiProject.Db/dbo/Stored Procedures/Order_Service_Worker_DeleteById.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[Order_Service_Worker_DeleteById]
+	@Id int
+AS
+DELETE [Order_Service_Worker]
+WHERE [Id]=@Id
