4376ee9d1cb7115eac9ec976f81c16680ba4fd36
DariaKostrik <dolorez0927@yandex.ru>
2023-03-06 19:40:42 +0300

    finish GetAllClientsTest and GetClientByIdTest

--- RabotygiProject.Bll.Test/ClientManagerTests.cs
+++ RabotygiProject.Bll.Test/ClientManagerTests.cs
@@ -1,10 +1,10 @@
 ﻿using Moq;
+using NUnit.Framework;
 using RabotyagiProject.Bll;
 using RabotyagiProject.Bll.Models;
+using RabotygiProject.Bll.Test.TestCaseSourse;
 using RabotyagiProject.Dal.Interface;
 using RabotyagiProject.Dal.Models;
-using RabotygiProject.Bll.Test.TestCaseSourse;
-using NUnit.Framework;
 
 namespace RabotygiProject.Bll.Test
 {
@@ -13,10 +13,50 @@ namespace RabotygiProject.Bll.Test
         private ClientManager _manager;
         private Mock<IClientRepository> _mock;
         [SetUp]
-            public void SetUp()
+        public void SetUp()
         {
             _mock = new Mock<IClientRepository>();
             _manager = new ClientManager(_mock.Object);
         }
+
+        [TestCaseSource(typeof(GetAllClientsTestCaseSourse))]
+        public void GetAllClientsTest(List<ClientDto> clientDto, List<ClientOutputModel> modelClient)
+        {
+            _mock.Setup(o => o.GetAllClients()).Returns(clientDto).Verifiable();
+            List<ClientOutputModel> actual = _manager.GetAllClients();
+            List<ClientOutputModel> expected = new List<ClientOutputModel>(modelClient);
+            _mock.VerifyAll();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCaseSource(typeof(GetClientByIdTestCaseSourse))]
+        public void GetClientByIdTest(ClientDto clientDto, ClientOutputModel modelClient, int id)
+        {
+            _mock.Setup(o => o.GetClientById(id)).Returns(clientDto).Verifiable();
+            ClientOutputModel actual = _manager.GetClientById(id);
+            ClientOutputModel expected = modelClient;
+
+            _mock.VerifyAll();
+            Assert.AreEqual(expected, actual);
+        }
+
+        //[TestCaseSource(typeof(UpdateBusyTimeByIdTestCaseSourse))]
+        //public void UpdateBusyTimeByIdTest(BusyTimeDto dtoBusyTime, BusyTimeInputModel modelBusyTime)
+        //{
+        //    BusyTimeDto expected = dtoBusyTime;
+        //    _mock.Setup(o => o.UpdateBusyTimeById(dtoBusyTime)).Verifiable();
+        //    _manager.UpdateBusyTimeById(modelBusyTime);
+        //    _mock.Verify();
+        //}
+
+        //[TestCaseSource(typeof(AddNewBusyTimeTestCaseSourse))]
+        //public void AddNewBusyTimeTest(BusyTimeDto dtoBusyTime, BusyTimeInputModel modelBusyTime)
+        //{
+        //    BusyTimeDto expected = dtoBusyTime;
+        //    _mock.Setup(o => o.AddNewBusyTime(dtoBusyTime)).Verifiable();
+        //    _manager.AddNewBusyTime(modelBusyTime);
+        //    _mock.Verify();
+        //}
     }
 }
+
--- RabotygiProject.Bll.Test/TestCaseSourse/ClientManagerTestsCaseSourses.cs
+++ RabotygiProject.Bll.Test/TestCaseSourse/ClientManagerTestsCaseSourses.cs
@@ -1,12 +1,267 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
+//using System.Collections.Generic;
+using RabotyagiProject.Bll.Models;
+using RabotyagiProject.Dal.Models;
 
-namespace RabotygiProject.Bll.Test.TestCaseSourse
+public class GetAllClientsTestCaseSourse : IEnumerable
 {
-    public class ClientManagerTestsCaseSourses
+    IEnumerator IEnumerable.GetEnumerator()
     {
+        List<ClientDto> clientDto = new List<ClientDto>();
+        List<ClientOutputModel> modelClient = new List<ClientOutputModel>();
+        List<ServiceWorkerOutputModel> Services;
+        List<OrderDto> OrderDto = new List<OrderDto>()
+        {
+            new OrderDto()
+            {
+                Id=2,
+                ClientId = 2,
+                IsCompleted =true,
+                Adress = "Ulitsa 1905 goda, 5, 2, 121",
+                Date = DateTime.Parse("10.02.2023"),
+                Cost = 120001,
+                Rate = (RabotyagiProject.Dal.Options.Rate?)4,
+                Report = "klass",
+                Services = new List <ServiceWorkerDto>()
+                {
+                    new ServiceWorkerDto()
+                    {
+                        ServiceId=1,
+                        WorkerId =null,
+                        Workload =3
+                    },
+                    new ServiceWorkerDto()
+                    {
+                        ServiceId =1,
+                        WorkerId =2,
+                        Workload =3
+                    }
+                },
+            },
+            new OrderDto()
+            {
+                    Id=2,
+                    ClientId = 2,
+                    IsCompleted =false,
+                    Adress = "Ulitsa Lenina, 3, 10, 1",
+                    Date = DateTime.Parse("15.10.2023"),
+                    Cost = 30000,
+                    Rate = null,
+                    Report = null,
+
+            }
+        };
+        List<OrderOutputModel> OrderModel = new List<OrderOutputModel>()
+        {
+            new OrderOutputModel()
+            {
+                Id=2,
+                ClientId = 2,
+                IsCompleted =true,
+                Adress = "Ulitsa 1905 goda, 5, 2, 121",
+                Date = DateTime.Parse("10.02.2023"),
+                Cost = 120001,
+                Rate = (RabotyagiProject.Dal.Options.Rate?)4,
+                Report = "klass",
+                Services = new List <ServiceWorkerOutputModel>()
+                {
+                    new ServiceWorkerOutputModel()
+                    {
+                        ServiceId=1,
+                        WorkerId =null,
+                        Workload =3
+                    },
+                    new ServiceWorkerOutputModel()
+                    {
+                        ServiceId =1,
+                        WorkerId =2,
+                        Workload =3
+                    }
+                },
+            },
+            new OrderOutputModel()
+            {
+                    Id=2,
+                    ClientId = 2,
+                    IsCompleted =false,
+                    Adress = "Ulitsa Lenina, 3, 10, 1",
+                    Date = DateTime.Parse("15.10.2023"),
+                    Cost = 30000,
+                    Rate = null,
+                    Report = null,
+                    Services = new List<ServiceWorkerOutputModel>(),
+            }
+        };
+
+        clientDto = new List<ClientDto>()
+            {
+                new ClientDto()
+                {
+                Id = 1,
+                Name = "Ivanov Ivan Ivanovich",
+                Phone = "79159999999",
+                Mail = "iii@gmail.ru",
+                IsDeleted = false,
+                Orders =OrderDto
+                }
+        };
+
+        modelClient = new List<ClientOutputModel>()
+            {
+                new ClientOutputModel()
+                {
+                Id = 1,
+                Name = "Ivanov Ivan Ivanovich",
+                Phone = "79159999999",
+                Mail = "iii@gmail.ru",
+                Orders = OrderModel
+                }
+            };
+        yield return new Object[] { clientDto, modelClient };
+
+        yield return new Object[]
+        {
+            clientDto = new List<ClientDto>(),
+            modelClient = new List<ClientOutputModel>()
+        };
+
+        clientDto = new List<ClientDto>()
+                {
+                    new ClientDto()
+                    {
+                     Id = 1,
+                     Name = "Ivanov Ivan Ivanovich",
+                     Phone = "79159999999",
+                     Mail = "iii@gmail.ru",
+                     IsDeleted =false
+                    },
+                    new ClientDto()
+                    {
+                        Id = 2,
+                        Name = "Petrov Petr Petrovich",
+                        Phone = "79111111111",
+                        Mail = "ppp@gmail.ru",
+                        IsDeleted=false
+                    }
+        };
+        modelClient = new List<ClientOutputModel>()
+                {
+                    new ClientOutputModel()
+                    {
+                     Id = 1,
+                     Name = "Ivanov Ivan Ivanovich",
+                     Phone = "79159999999",
+                     Mail = "iii@gmail.ru",
+                    },
+                    new ClientOutputModel()
+                    {
+                        Id = 2,
+                        Name = "Petrov Petr Petrovich",
+                        Phone = "79111111111",
+                        Mail = "ppp@gmail.ru",
+                    }
+                };
+        yield return new Object[] { clientDto, modelClient };
     }
 }
+
+
+
+
+
+
+
+public class GetClientByIdTestCaseSourse : IEnumerable
+{
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        ClientDto clientDto = new ClientDto();
+        ClientOutputModel modelClient = new ClientOutputModel();
+        List <ServiceWorkerOutputModel> Services;
+        List <OrderDto> OrderDto1 = new List <OrderDto>()
+        {
+            new OrderDto()
+            {
+                Id=2,
+                ClientId = 2,
+                IsCompleted =true,
+                Adress = "Ulitsa 1905 goda, 5, 2, 121",
+                Date = DateTime.Parse("10.02.2023"),
+                Cost = 120001,
+                Rate = (RabotyagiProject.Dal.Options.Rate?)4,
+                Report = "klass",
+                Services = new List <ServiceWorkerDto>()
+                {
+                    new ServiceWorkerDto()
+                    {
+                        ServiceId=1,
+                        WorkerId =null,
+                        Workload =3
+                    },
+                    new ServiceWorkerDto()
+                    {
+                        ServiceId =1,
+                        WorkerId =2,
+                        Workload =3
+                    }
+                }
+            }
+        };
+        List <OrderOutputModel> OrderModel1 = new List <OrderOutputModel>()
+        {
+            new OrderOutputModel()
+            {
+                Id=2,
+                ClientId = 2,
+                IsCompleted =true,
+                Adress = "Ulitsa 1905 goda, 5, 2, 121",
+                Date = DateTime.Parse("10.02.2023"),
+                Cost = 120001,
+                Rate = (RabotyagiProject.Dal.Options.Rate?)4,
+                Report = "klass",
+                Services = new List <ServiceWorkerOutputModel>()
+                {
+                    new ServiceWorkerOutputModel()
+                    {
+                        ServiceId=1,
+                        WorkerId =null,
+                        Workload =3
+                    },
+                    new ServiceWorkerOutputModel()
+                    {
+                        ServiceId =1,
+                        WorkerId =2,
+                        Workload =3
+                    }
+                },
+            }
+        };
+
+    int Id = 1;
+    clientDto = new ClientDto()
+        {
+            Id = 1,
+            Name = "Ivanov Ivan Ivanovich",
+            Phone = "79159999999",
+            Mail = "iii@gmail.ru",
+            IsDeleted = false,
+            Orders = OrderDto1
+    };
+        ClientOutputModel clientOutputModel = new ClientOutputModel()
+        {
+            Id = 1,
+            Name = "Ivanov Ivan Ivanovich",
+            Phone = "79159999999",
+            Mail = "iii@gmail.ru",
+            Orders = OrderModel1
+        };
+yield return new Object[] { clientDto, clientOutputModel, Id };
+
+int Id2 = 2;
+ClientDto clientDto2 = new ClientDto();
+ClientOutputModel сlientOutputModel2 = new ClientOutputModel();
+yield return new Object[] { clientDto2, сlientOutputModel2, Id2 };
+    }
+}
+    
+
